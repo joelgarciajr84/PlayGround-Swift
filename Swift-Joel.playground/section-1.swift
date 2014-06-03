8ec3ed2def2778d773117c78e121f702fc79f44f
Joel Garcia Jr <joel.garciajr84@gmail.com>
2014-06-03 14:58:11 -0300

    Commit Inicial

---
+++ Swift-Joel.playground/section-1.swift
@@ -0,0 +1,69 @@
+//Aprendizado e Exercicios Swift
+
+//Variaveis e constantes
+
+var num1 = 10
+
+var num2 = 20
+
+let total = num1 + num2
+
+let frasetotal = "O total é \(total)"
+
+//Arrays
+
+var sistemas = ["MacOs", "Windows", "Linux"]
+
+var criadores = ["Bill Gates", "Steve Jobs", "Linus Torvalds"]
+
+//Loops com for e switch
+
+for sistema in sistemas{
+    
+    switch sistema {
+        
+        case "MacOs":
+        
+        println(sistema, "The Best")
+        
+    case "Windows":
+        
+        println(sistema, "Time to Restart")
+        
+    case "Linux":
+        
+        println(sistema, "Nice")
+        
+    default:
+        
+        println("Some error")
+    }
+    
+}
+
+    for criador in criadores{
+        
+        switch criador {
+            
+            case "Bill Gates":
+            
+                println(criador, "I'm Rich")
+            
+            case "Steve Jobs":
+            
+                println(criador, "I'm Genius")
+            
+            case "Linus Torvalds":
+            
+            
+                println(criador, "I'm a Hacker")
+        
+        
+        default:
+            
+            println("Some error")
+            
+        }
+}
+
+
